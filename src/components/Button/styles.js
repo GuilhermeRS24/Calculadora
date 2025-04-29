8bc1007956a8abbe4b7cf595cc2d00b0e1114557
Guilherme Rodrigues Santos <guilherme.rs2407@gmail.com>
2025-04-29 20:24:25 -0300

    Primeiro commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button `
+    padding:20px;
+    background-color:rgb(179, 243, 204);
+    color: #000000;
+    border: 1px solid #CDCDCD
+
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+      cursor: pointer;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div `
+    width: 100%;
+    height: 75px;
+
+
+
+    display: flex;
+    align-items:center;
+    justify-content: flex-end;
+
+
+    input {
+        font-size:24px;
+        font-family:'Roboto';
+        color: #f6f6f6;
+
+        width: 100%;
+        height: 75px;
+        background-color:rgb(161, 221, 231);
+        border:0;
+        padding: 0 10px;
+    };
+`
