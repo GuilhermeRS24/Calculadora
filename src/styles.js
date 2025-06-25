12676318e3bec8b36409e21eefd60588974b7297
Guilherme Rodrigues Santos <guilherme.rs2407@gmail.com>
2025-06-25 18:12:43 -0300

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,27 +1,54 @@
 import styled from 'styled-components';
 
 export const Container = styled.div `
-
-    width:  100%;
-    height: 100vh;
-    background-color:rgb(78, 78, 78);
     
-    display: flex;
+    background-color: #f6f6f6;
+   color: #1c4c32;
+    display:flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
 `
 
-export const Content = styled.div`
-    background-color:rgb(15, 208, 233);
-    width: 50%;
-    min-height: 350px;
-    
+export const Header = styled.div `
+    color: #f6f6f6;
+    margin: 5px 0 30px 0;
+    width: 97%;
+    height: 115px;
+    background-color: #407c54;
+    box-shadow: inset 0 0 0 5px #1c4c32;
+    display: flex;
+    justify-content: space-between;
+    padding: 15px;
+    border-radius: 5px;
+}
 `
 
-export const Row = styled.div`
-  display:flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+export const Main = styled.div `
+    border-radius: 10px;
+    width: 1000px;
+    height: 100vh;
+    background-color: #407c54;
+    color: #f6f6f6;
+
+    box-shadow: inset 0 0 0 5px #1c4c32;
+    padding: 30px;
+
+    display:flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+export const Paragrafo  = styled.div `
+    margin: 8px 10px 25px 10px;
+    color #000000;
+`
+export const Footer = styled.div `
+    width: 100%;
+    height: 20vh;
+    background-color: #1c4c32;
+    color: #f6f6f6;
+    display:flex;
+    justify-content: space-around;
+    align-items:center;
 `
 
