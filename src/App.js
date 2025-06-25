95a28671d2cbdc610319a8f43a37a4561c6c4d82
Guilherme Rodrigues Santos <guilherme.rs2407@gmail.com>
2025-06-25 18:12:11 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,128 +1,207 @@
-import {Container, Content, Row} from './styles';
-import Input from './components/Input'
-import Button from './components/Button'
+import logo from './assets/logo.png';
+import menuhamburguer from './assets/menuhamburguer.png';
+import whatsapp from './assets/whatsapp.png'
+import alvenaria_icon from './assets/alvenaria_icon.png'
+import eletricidade_icon from './assets/eletricidade_icon.png'
+import encanamento_icon from './assets/encanamento_icon.png'
+import drywall_icon from './assets/drywall_icon.png'
+import ar_icon from './assets/ar_icon.png'
+import facebook from './assets/facebook.png'
 import { useState } from 'react';
-  
-const App = () => {
-  const [currentNumber, setCurrentNumber] = useState('0');
-  const [firstNumber, setFirstNumber] = useState('0');
-  const [operation, setOperation] = useState('');
+import {Container, Header, Main, Paragrafo, Footer} from './styles';
+import './global';
 
-  const handleOnClear = () => {
-    setCurrentNumber('0')
-    setFirstNumber('0')
-    setOperation('')
-  };
+function App() {
 
-  const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
+  const clickMenu = () => {
+    window.scroll({top: 0, behavior: 'smooth'})
   }
 
-  const handleSumNumbers = () => {
-
-    if(firstNumber === '0'){
-        setFirstNumber(String(currentNumber));
-        setCurrentNumber('0')
-        setOperation('+')
-    }else {
-      const sum = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(String(sum))
-      setOperation('')
-    }
+  const icon = {
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center'
   }
 
-  const handleLessNumbers = () =>{//Diminuir (-)
-    if(firstNumber === '0'){
-      setFirstNumber(String(currentNumber));
-      setCurrentNumber('0');
-      setOperation('-')
-    }else{
-      const Less = Number(firstNumber) - Number(currentNumber);
-      setCurrentNumber(String(Less))
-      setOperation('')
-    };
-  }
-
-  const handleMultiplyNumbers = () =>{//Multiplicar (x)
-    if(firstNumber === '0'){
-      setFirstNumber(String(currentNumber));
-      setCurrentNumber('0');
-      setOperation('x')
-    }else{
-      const Multiply = Number(firstNumber) * Number(currentNumber);
-      setCurrentNumber(String(Multiply))
-      setOperation('')
-    }
-  }
-
-  const handleDivisionNumbers = () =>{//Dividir (/)
-    if(firstNumber === '0'){
-      setFirstNumber(String(currentNumber));
-      setCurrentNumber('0');
-      setOperation('/')
-    }else{
-      const Division = Number(firstNumber) / Number(currentNumber);
-      setCurrentNumber(String(Division))
-      setOperation('')
-    }
-  }
-
-  const handleEquals = () => {//Igual (=)
-    if(firstNumber !== '0' && operation !== ''  && currentNumber !== '0'){
-      switch(operation){
-        case '+':
-          handleSumNumbers();
-          break;
-        case '-':
-          handleLessNumbers();
-          break;
-          case 'x':
-            handleMultiplyNumbers();
-          break;
-          case '/':
-            handleDivisionNumbers();
-          break;
-        default: 
-          break;
-
-      }
-    }
-  }
   return (
-    <Container>
-      
-      <Content> 
-        <Input value={currentNumber}/>
-        <Row>
-          <Button label={'7'} onClick={()=>handleAddNumber('7')}/>
-          <Button label={'8'} onClick={()=>handleAddNumber('8')}/>
-          <Button label={'9'} onClick={()=>handleAddNumber('9')}/>
-          <Button label={'+'} onClick={handleSumNumbers}/>
-        </Row>
-        <Row>
-          <Button label={'4'} onClick={()=>handleAddNumber('4')}/>
-          <Button label={'5'} onClick={()=>handleAddNumber('5')}/>
-          <Button label={'6'} onClick={()=>handleAddNumber('6')}/>
-          <Button label={'-'} onClick={handleLessNumbers}/>
-        </Row>
-        <Row>
-          <Button label={'1'} onClick={()=>handleAddNumber('1')}/>
-          <Button label={'2'} onClick={()=>handleAddNumber('2')}/>
-          <Button label={'3'} onClick={()=>handleAddNumber('3')}/>
-          <Button label={'x'} onClick={handleMultiplyNumbers}/>
-        </Row>
-        <Row>
-          <Button label={'C'} onClick={handleOnClear}/>
-          <Button label={'0'} onClick={()=>handleAddNumber('0')}/>
-          <Button label={','} onClick={()=>handleAddNumber('.')}/>
-          <Button label={':'} onClick={handleDivisionNumbers}/>
-        </Row>
-        <Row>
-        <Button label={'='} onClick={handleEquals}/>
-        </Row>
-      </Content>
-     
-    </Container>
+    <div className="App">
+      <Container>
+        <button onClick={clickMenu} style={{//Botão para Retornar ao inicio da pagina.
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        padding: "10px 15px",
+        backgroundColor: "#407c54",
+        color: "white",
+        border: 'none',
+        borderRadius: "5px",
+        cursor: "pointer",
+      }}
+    >
+      ↑ Início
+    </button>
+        <Header>
+          <img src={logo} className="Rs-logo" alt="logo" style={{//Logo da RS
+            margin: '0 0 0 30px',
+            width: '10%',
+            height: '100%',
+            objectFit: 'cover'
+          }}/>
+          <div style={{display:'flex',flexDirection: 'column', justifyContent:'flex-end'}}>
+            <h style={{ fontSize: '35px', margin:'25px 0px' }}>RS Manuntenção e Reformas</h>
+
+            <div style={{margin:'0 50px 15px 0', display:'flex',flexDirection: 'row'}}>
+              <p>Contatos:&nbsp;</p>
+               <p>
+                   41 9890-4364
+              </p>
+              <p>&nbsp;| Email: rs.ManuntençãoReformas | Instagram: @RS_ManuntençãoReformas</p>
+            </div>  
+          </div>
+           
+            <a href="https://wa.me/5547999746021?text=Olá" target="_blank" rel="noopener noreferrer">
+                <img src={menuhamburguer} className='menuhamburguer' alt='menu' style={{
+              width:'100%',
+              height: '40%',
+              objectFit: 'cover'
+            }}/>
+              </a>
+        </Header>
+        <h1 style={{fontSize:'28px',margin:'15px'}}>Alguns dos trabalhos que fazemos:</h1>
+        <div style={{width:'70%',height: '30vh',display:'flex', justifyContent: 'space-evenly',alignItems: 'center',backgroundColor:'#407c54',color:'#f6f6f6',borderRadius:'15px'}}>  
+
+            <a href='#alvenaria' style={icon}>
+              <img src={alvenaria_icon} />Alvenaria</a>
+            
+            <a href='#eletrica' style={icon}>
+              <img src={eletricidade_icon} />Elétrica
+            </a>
+
+            <a href='#encanamento' style={icon}>
+              <img src={encanamento_icon} />Encanamento
+            </a>
+
+            <a href='#drywall' style={icon}>
+              <img src={drywall_icon} />Drywall
+            </a>
+
+            <a href='#ar-condicionado' style={icon}>
+              <img src={ar_icon} />Ar-Condicionado
+            </a>
+        </div>
+        <a href="https://wa.me/5547999746021?text=Estou interessado na Alvenaria" target="_blank" rel="noopener noreferrer"
+            style={{margin:'40px',width:'400px',height:'50px' , fontSize:'34px', backgroundColor:'#407c54', color:'#f6f6f6', borderRadius:'5px',boxShadow:'inset 0 0 0 2px #1c4c32', display:'flex',alignItems:'center',justifyContent:'center'}}>
+            Entrar em contato
+            </a>
+        <div style={{padding:'50px'}}>
+        <h style={{fontSize:'35px'}}>RS Manuntenção e Reformas</h>
+
+        <Paragrafo>&nbsp; Na RS Manuntenção e Reformas, trabalhamos com projetos em diferentes tipos de imóveis, sempre buscando oferecer soluções práticas e eficientes para nossos clientes. Atendemos tanto residências quanto prédios e comércios, adaptando nossos serviços às necessidades de cada tipo de espaço.
+        </Paragrafo>
+            
+        <h1>Casas:</h1>
+        <Paragrafo>
+        - Nossa equipe está preparada para realizar reformas, manutenções e até construções, sempre focando em melhorar o conforto e a funcionalidade do ambiente, com o cuidado de respeitar as preferências de quem mora no local.
+        </Paragrafo>
+
+        <h1>Prédios:</h1>
+        <Paragrafo>
+        - Oferecemos serviços de manutenção, reformas e adaptações para condomínios ou empreendimentos maiores. Nosso objetivo é entregar resultados que garantam segurança e qualidade, com respeito aos prazos e orçamentos.
+        </Paragrafo>
+
+        <h1>Comércio:</h1>
+        <Paragrafo>
+        - Sabemos da importância de um ambiente bem estruturado para os negócios. Trabalhamos em reformas e adequações de espaços comerciais, sempre buscando otimizar a funcionalidade e criar um ambiente que atenda às necessidades dos nossos clientes e de seus consumidores.
+        </Paragrafo>
+
+        <Paragrafo>     
+        &nbsp;Nossa empresa se dedica a entregar um serviço de qualidade, com atenção ao detalhe e sempre dentro do que foi combinado. Estamos prontos para ajudar em qualquer tipo de obra, seja em casa, em prédios ou no comércio.
+        </Paragrafo>
+        </div>
+
+        <section id="alvenaria" className="relative h-64 bg-gray-100" style={{margin:'50px',display:'flex', flexDirection:'column', alignItems:'center'}}>
+          <h style={{ margin:'30px', fontSize:'50px' }}>Alvenaria</h>
+          <Main>
+            <a href="https://wa.me/5547999746021?text=Estou interessado na Alvenaria" target="_blank" rel="noopener noreferrer"
+            style={{width:'35%',height:'8%' , fontSize:'34px', backgroundColor:'#f6f6f6', color:'#1c4c32', borderRadius:'5px',boxShadow:'inset 0 0 0 2px #1c4c32', display:'flex',alignItems:'center',justifyContent:'center'}}>
+            Estou interessado
+            </a>
+
+
+          </Main>
+        </section>
+
+        <section id="eletrica" style={{margin:'50px',display:'flex', flexDirection:'column', alignItems:'center'}}>
+          <h style={{ margin:'30px', fontSize:'50px' }}>Elétrica</h>
+          <Main>
+
+
+
+          </Main>
+        </section>
+
+        <section id="encanamento" style={{margin:'50px',display:'flex', flexDirection:'column', alignItems:'center'}}>
+          <h style={{ margin:'30px', fontSize:'50px' }}>Encanamento</h>
+          <Main>
+
+
+
+          </Main>
+        </section>
+
+        <section id="drywall" style={{margin:'50px',display:'flex', flexDirection:'column', alignItems:'center'}}>
+          <h style={{ margin:'30px', fontSize:'50px' }}>Drywall</h>
+          <Main>
+
+
+
+          </Main>
+        </section>
+
+        <section id="ar-condicionado" style={{margin:'50px',display:'flex', flexDirection:'column', alignItems:'center'}}>
+          <h style={{ margin:'30px', fontSize:'50px' }}>Ar-Condicionado</h>
+          <Main>
+
+
+
+          </Main>
+        </section>
+        <Footer>
+          <img src={logo} className="Rs-logo" alt="logo" style={{//Logo da RS
+            margin: '0 0 0 30px',
+            width: '80px',
+            height: '80px',
+            objectFit: 'cover'
+          }}/>
+            <div>
+              
+              <h1>
+                Contatos:
+              </h1>
+              <a href="https://wa.me/5547999746021?text=Olá" target="_blank" rel="noopener noreferrer">
+                <img src={whatsapp}/>
+              </a>
+              <a href='https://www.facebook.com/cleciorenators?locale=pt_BR' target='_blank' rel="noopener noreferrer">
+                <img src={facebook}/>
+              </a>
+            </div>
+            <div style={{display:'flex',flexDirection:'row'}}>
+            <h1>Serviços</h1>
+              <ul>
+            <a href='#alvenaria' style={icon}>Alvenaria</a>
+            
+            <a href='#eletrica' style={icon}>Elétrica</a>
+
+            <a href='#encanamento' style={icon}>Encanamento</a>
+
+            <a href='#drywall' style={icon}>Drywall</a>
+
+            <a href='#ar-condicionado' style={icon}>Ar-Condicionado</a>
+              </ul>
+            </div>
+        </Footer>
+      </Container>
+    </div>
   );
 }
 
