a1dbcea77687b446daead5358504ed09c9ea80c2
Guilherme Rodrigues Santos <guilherme.rs2407@gmail.com>
2025-06-25 18:12:26 -0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,8 +1,21 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
-export default createGlobalStyle `
-    * body {
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Huninn&display=swap" rel="stylesheet');
+      a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  a:hover {
+    font-size: 22px;
+  }
+
+  * {
+
     margin: 0;
     padding: 0;
-    }
-`
+    font-family: 'Huninn', sans-serif;
+    font-size: 20px
+  }
+`;
